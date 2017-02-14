538b83e7f1beaad15b2c6fb68f8ca5fae745cb19
Easy-wei <easy-we@outlook.com>
2017-02-14 16:47:17 +0800

    16:47

--- 临时2.js
+++ 临时2.js
@@ -1,72 +1,66 @@
  do {
-     var e = math_tools.rand_int(2, 9);
-     var q = math_tools.rand_int(-50, 50, [-1, 0, 1]);
-     var g = math_tools.rand_int(-50, 50, [-1, 0, 1]);
-     var h = math_tools.rand_int(-50, 50, [-1, 0, 1]);
-     var root = math_tools.polynomial(e, q, 0, h); //一元多次方程求根
-     root = Number(root[0]);
+     var v1 = math_tools.rand_int(2, 9);
+     var v2 = math_tools.rand_int(-50, 50, [-1, 0, 1]);
+     var v3 = math_tools.rand_int(-50, 50, [-1, 0, 1]);
+     var v4 = math_tools.rand_int(-50, 50, [-1, 0, 1]);
+     var root = math_tools.polynomial(1, 0, v3, v4); //一元多次方程求根
+     root = Number(root.sort()[root.length - 1]);
      var range_start = Math.floor(root);
      var range_end = range_start + 1;
  }
  while (isNaN(root) || (range_start * range_end) === 0 || f(range_start) * f(range_end) === 0);
+ var range_near = (math.abs(f(range_start)) > math.abs(f(range_end))) ? range_end : range_start;
+ var range_far = (math.abs(f(range_start)) < math.abs(f(range_end))) ? range_end : range_start;
 
  function f(x) {
-     answer = e * math.pow(x, 3) + q * x * x + h;
+     var answer = math.pow(x, 3) + v3 * x + v4;
      return answer;
  }
- var equation = e + `x^{3}` + q.signed() + `x^2` + h.signed();
+
+ function g(x) {
+     var answer = 3 * math.pow(x, 2) + v3;
+     return answer;
+ }
+ var equation = `x^{3}` + v3.signed() + `x` + v4.signed();
+ var equation1 = `3x^2` + v3.signed();
 
  self.ordered_answer = true;
  self.precision = 4;
  var ans;
- var arry = [];
 
+ var a = Math.floor(root);
+ var b = a + 1;
+ var x = math_tools.round(root, 0);
 
- function linear_interpolation(x, y) { //linear interpolation
-     var a = x;
-     var b = y;
-     var c;
+ function diff_iteration(x) { //differential_interation
+     var x1 = x;
      var arry = [
-         [`a`, `f(a)`, `b`, `f(b)`, `c`, `f(c)`]
+         [tex(`x`), tex(`f(x)`), tex(`f'(x)`), tex(`x^{}_n`)]
      ];
      for (var i = 0; i < 10; i++) {
-         c = (a * math.abs(f(b)) + b * math.abs(f(a))) / (math.abs(f(a)) + math.abs(f(b)));
          var arry1 = [];
-         arry1[0] = math.round(a, 3);
-         arry1[1] = math.round(f(a), 3);
-         arry1[2] = math.round(b, 3);
-         arry1[3] = math.round(f(b), 3);
-         arry1[4] = math.round(c, 3);
-         arry1[5] = math.round(f(c), 3);
+         x1 = x - f(x) / g(x);
+         arry1[0] = math.round(x, 4);
+         arry1[1] = math.round(f(x), 4);
+         arry1[2] = math.round(g(x), 4);
+         arry1[3] = math.round(x1, 4);
          arry.push(arry1);
-         if (f(a) * f(c) < 0) {
-             if (math.round(b, 1) == math.round(c, 1)) {
-                 ans = math.round(c, 1);
-                 break;
-             }
-             a = a;
-             b = c;
+         if (math_tools.round(x, 2) == math_tools.round(x1, 2)) {
+             ans = math_tools.round(x, 2);
+             break;
          }
-         if (f(b) * f(c) < 0) {
-             if (math.round(a, 1) == math.round(c, 1)) {
-                 ans = math.round(c, 1);
-                 break;
-             }
-             a = c;
-             b = b;
-         }
+         x = x1;
      }
-     ans = math.round(c, 1);
      return arry;
  }
-
- var arry = linear_interpolation(range_start, (range_start + 1));
+ var arry = diff_iteration(x);
  var arry2 = [];
  for (var i = 1; i < arry.length; i++) {
      for (var j = 0; j < arry[i].length; j++) {
          arry2.push(arry[i][j]);
      }
  }
+
  var rows = 0;
  input_change = function () {
 
@@ -125,24 +119,24 @@
  self.text = function () {
      return `<style media="screen" type="text/css"> table.input_table, table.input_table td {border: 1px solid #444;border-collapse: collapse;} .input_table .book_math_input {border: none;}</style>` +
 
-         `One root of the equation ` + tex(equation) + ` lie in the interval[` + range_start + `,` + range_end + `] ` +
+         `Show that the equation ` + tex(equation + `=0`) + ` has a root between ` + range_start + ` and ` + range_end +
 
-         `<br>Use linear interpolation to find the root of to 1 decimal place.<br>` +
+         `<br>Find the root correct to two decimal places using the Newton-Raphson process.<br>` +
 
          `(if the input is irrational number,Keeping four decimal places)<br>` +
 
-         `<p class="book_button" ontouchstart="touch_start(event)" onclick="add_row(6)">Add row</p> 
-           <p id="remove_row_button" class="book_button" ontouchstart="touch_start(event)" onclick="remove_row(6)">Remove row</p>
+         `<p class="book_button" ontouchstart="touch_start(event)" onclick="add_row(4)">Add row</p> 
+           <p id="remove_row_button" class="book_button" ontouchstart="touch_start(event)" onclick="remove_row(4)">Remove row</p>
           <table id="table" class='input_table'>
-          <tr><td><b>` + tex(`a`) + `</b></td><td><b>` + tex(`f(a)`) + `</b></td><td><b>` + tex(`b`) +
-         `</b></td><td><b>` + tex(`f(b)`) + `</b></td><td><b>` + tex(`c`) + `</b></td><td><b>` + tex(`f(c)`) + `</b></td></tr>
-          </table>` + `<br> `;
+          <tr><td><b>` + tex(`x`) + `</b></td><td><b>` + tex(`f(x)`) + `</b></td><td><b>` + tex(`f'(x)`) +
+         `</b></td><td><b>` + tex(`x^{}_n`) + `</b></td></tr>
+          </table>`;
 
  };
 
 
  self.post_load = function () {
-     add_row(6);
+     add_row(4);
      // Hide the remove button since we only have 1 row
      $("#remove_row_button").hide();
  };
@@ -151,42 +145,38 @@
      steps = [
          `Let ` + tex(`f(x)=` + equation),
 
-         `First we will check that there is a root of the equation ` + tex(equation) + ` between ` + tex(`x=` + range_start) + ` and ` + tex(`x=` + range_end),
-
-         tex(`f(` + range_start + `)=` + f(range_start)) + tex(`\\,\\,\\,f(` + range_end + `)=` + f(range_end)),
+         tex(`\\therefore\\,f(` + range_start + `)=` + f(range_start)) + tex(`\\,\\,\\,f(` + range_end + `)=` + f(range_end)),
 
          (f(range_start) > 0) ? tex(`f(` + range_start + `)` + `>0>` + `f(` + range_end + `)`) : tex(`f(` + range_start + `)` + `<0<` + `f(` + range_end + `)`),
 
-         `There is a change of sign and the equation is continuous in this range, so there must be a root between ` + tex(`` + range_start) + ` and ` + tex(`` + range_end),
+         `Since there is a change of a sign between ` + tex(`f(` + range_start + `)`) + ` and ` + tex(`f(` + range_end + `)`),
 
-         `We will label these bounds as ` + tex(`a=` + range_start) + ` and ` + tex(`b=` + range_end),
+         `equation ` + tex(equation + `=0`) + ` has a root in the interval[` + range_start + `,` + range_end + `]`,
 
-         `To obtain a more accurate estimate of the value of this root, we need to reduce the size of the interval that it is within`,
+         `then using the Newton-Raphson process to work out ` + tex(`x^{}_1`) + `<br>`,
 
-         `Following the method of linear interpolation, then using similar triangles to work out ` + tex(`x^{}_1`),
+         (math.abs(f(range_start)) > math.abs(f(range_end))) ? tex(`\\because\\,\\,` + `|f(` + range_start + `)|>|f(` + range_end + `)|`) + `<br>` +
 
-         tex(`\\dfrac{` + range_end + `-x^{}_1}{x^{}_1` + (-range_start).signed() + `}=|\\dfrac{f(` + range_end + `)}{f(` + range_start + `)}|`),
+         tex(`\\therefore\\,\\,` + range_end) + ` is closer to the correct answer` :
+         tex(`\\because\\,\\,` + `|f(` + range_start + `)|<|f(` + range_end + `)|`) + `<br>` +
+         tex(`\\therefore\\,\\,` + range_start) + ` is closer to the correct answer`,
 
-         tex(`\\therefore\\,\\,x^{}_1=\\,` + arry[1][4]),
 
-         tex(`f(` + arry[1][4] + `)=` + arry[1][5]),
+         `Using ` + tex(`x^{}_0=` + range_near),
 
-         `Using the interval [` + math_tools.round(arry[1][4], 4) + ` , ` + range_end + `]`,
+         tex(`x^{}_1=x^{}_0-\\dfrac{f(` + range_near + `)}{f'(` + range_near + `)}`),
 
-         `Make the table of values. Let the interval (a,b) be the interval in which the root lies. `,
+         (f(range_near) * g(range_near) > 0) ? tex(`x^{}_1=` + range_near + `-`) + math_visuals.fraction.tex(f(range_near), g(range_near)) :
+         tex(`x^{}_1=` + range_near + `+`) + math_visuals.fraction.tex(-f(range_near), g(range_near)),
 
-         math_visuals.array_to_table(arry),
+         tex(`x^{}_1=` + arry[1][3]),
 
-         `Having followed the method ` + tex(`` + (arry.length - 1)) + ` times, we know that there is a root within the range ` +
+         `Then using the same way to make the table of values. Let the interval (a,b) be the interval in which the root lies. `,
 
-         tex((arry[(arry.length - 1)][5] > 0 ? ((arry[(arry.length - 1)][1] > 0) ? `[` + (arry[(arry.length - 1)][4]) + `,` + arry[(arry.length - 1)][2] + `]` : `[` + arry[(arry.length - 1)][0] + `,` + arry[(arry.length - 1)][4] + `]`) :
-             ((arry[(arry.length - 1)][1] > 0) ? `[` + arry[(arry.length - 1)][0] + `,` + arry[(arry.length - 1)][4] + `]` : `[` + arry[(arry.length - 1)][4] + `,` + arry[(arry.length - 1)][2] + `]`))),
+         math_visuals.array_to_table(diff_iteration(x)),
 
-         tex(`\\because\\,` + arry[arry.length - 1][4] + ` \\thickapprox ` + math.round(arry[arry.length - 1][4], 1)) + ` and ` + tex(`\\,\\,\\,` + arry[arry.length - 2][4] + `\\thickapprox` + math.round(arry[arry.length - 1][4], 1)),
+         `Hence answer ` + ans + `<br>`,
 
-         `if ` + tex(arry[arry.length - 1][4] + ``) + ` to ` + tex(`1`) + ` decimal place equals ` + tex(`` + arry[arry.length - 2][4]) + ` to ` + tex(`1`) + ` decimal place `,
-
-         `so there must be a root of ` + tex(equation) + ` equal to ` + tex(`` + ans) + ` to ` + tex(`1`) + ` decimal place`,
      ];
      return steps[step];
  };
--- 自己写的function.js
+++ 自己写的function.js
@@ -440,3 +440,5 @@ math_tools["round"] = function (number, decimals) {
     }
     return s_x;
 };
+
+  (Tabulate your intermediate steps[exercise_only] as shown[/exercise_only].  Input values to ` + tex(`3`) + ` decimal places when required)
