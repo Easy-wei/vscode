e6d70203ca7016817e30aa2946ac29ff45dab8c3
shilei wei <shilei wei>
2017-03-11 14:09:38 +0800

    3.11

--- JS/临时.js
+++ JS/临时.js
@@ -1,293 +1,62 @@
-self.ordered_answer = true;
-self.precision = 2;
-
-var sum_m, sum_mx, sum_my;
-
-function centre_cal(array) {
-  var x = 0;
-  var y = 0;
-  var m = 0;
-  for (var i = 0; i < array.length; i++) {
-    m += array[i][0];
-    x += array[i][0] * array[i][1][0];
-    y += array[i][0] * array[i][1][1];
-  }
-  sum_m = m;
-  sum_mx = x;
-  sum_my = y;
-  x = x / m;
-  y = y / m;
-  var array1 = [];
-  array1.push(x, y);
-  return array1;
-}
-
-do {
-  var m1 = math_tools.rand_int(2, 6);
-  var m2 = math.randomInt(2, 6);
-  var m3 = math.randomInt(2, 6);
-  var m4 = math.randomInt(2, 6);
-  var x1 = math.randomInt(2, 6);
-  var y1 = math.randomInt(2, 6);
-  var array = [ //格式是：质量，坐标（x，y）。也就是[m,[x,y]]
-    [m1, [0, 0]],
-    [m2, [x1, 0]],
-    [m3, [x1, y1]],
-    [m4, [0, y1]],
-  ];
-  var coordinate = centre_cal(array);
-}
-while (coordinate[0] % 1 !== 0 || coordinate[1] % 1 !== 0);
-console.log(coordinate);
-self.text = function () {
-  return `<div id="graph_1"></div>A light retangular metal plate ~PQRS~ has ~PQ=` + array[1][1][0] + `cm ~ and ~PS=` + array[2][1][1] + `cm. ~ Particles of masses 
-
-~` + array[0][0] + `kg, ` + array[1][0] + `kg, ` + array[2][0] + `kg,~ and ~` + array[3][0] + `kg~ are attached respectively to the corner ~A,B,C, ~ and ~D~ of the plate.
-
-Find the distance of the centre mass of the loaded  plate form.
-		~\\bold{a)}~ the side ~PQ~ [answer] 
-        ~\\bold{b)}~ the side ~PS~ [answer]`;
+math_graphic={
+    graphic:function(array=[['g1'],[-6,6],[-6,6]]){//重新开图一定要注意这句话//array的必要格式为[[第一个是id变量,函数内部使用id],[坐标点1],[坐标点2]]
+		g1 = new graphic({'input_element': array[0][0], 'low_x': array[1][0],'high_x': array[1][1],'low_y': array[2][0],'high_y': array[2][1],});
+    },
+    draw:function(){
+		g1.draw();
+    },
+    grid:function(){
+		g1.add_element({'type': 'grid',});
+    },
+    dot:function(array=[[0,0]]){
+		g1.add_element({'type': 'dot','pos': array[0]});
+    },
+    label:function(array=[[-0.3,-0.3],['O']]){//[[id][坐标点1][要输入的标示量]]
+		g1.add_element({'type': 'label','pos': array[0],'text': array[1]});
+    },
+    line:function(array=[[0,0],[0,1]]){
+		g1.add_element({'type': 'line','start': array[0],'end': array[1]});
+    },
 };
 
+var v1=math.randomInt(3,6);
+var v2=math.randomInt(3,8);
+array_lines=[[0,0],[0,1],[2,1],[2,v1],[3,v1],[3,1],[v2,1],[v2,0]];
 
-coordinate_range = [-3, x1 + 3, -3, y1 + 3]; //调节坐标轴的大小比例
+function lines_make(array) {
+    for (var i = 1; i < array.length ; i++) {
+      g1.add_element({'type': 'line','start': array[i-1],'end': array[i]});
+    }
+    g1.add_element({'type': 'line','start': array[array.length-1],'end': array[0]});
+}
+
+var array_coordinate=[['g1'],[-2,8],[-2,9]];
+	self.text = function() {
+		return `What is ?<div id='g1'></div>
+		[answer]`;
+	};
 self.post_load = function () {
-  var g1 = new graphic({
-    'input_element': "graph_1",
-    'low_x': coordinate_range[0],
-    'high_x': coordinate_range[1],
-    'low_y': coordinate_range[2],
-    'high_y': coordinate_range[3]
-  });
-
-  g1.add_element({
-    'type': 'dot',
-    'pos': [array[0][1][0], array[0][1][1]]
-  });
-
-  g1.add_element({
-    'type': 'label',
-    'pos': [array[0][1][0] - 0.5, array[0][1][1] + 0.5],
-    'text': 'A'
-  });
-
-  g1.add_element({
-    'type': 'dot',
-    'pos': [array[1][1][0], array[1][1][1]]
-  });
-
-  g1.add_element({
-    'type': 'label',
-    'pos': [array[1][1][0], array[1][1][1] + 0.5],
-    'text': 'B'
-  });
-
-  g1.add_element({
-    'type': 'dot',
-    'pos': [array[2][1][0], array[2][1][1]]
-  });
-
-  g1.add_element({
-    'type': 'label',
-    'pos': [array[2][1][0] - 0.5, array[2][1][1] + 0.5],
-    'text': 'C'
-  });
-
-  g1.add_element({
-    'type': 'dot',
-    'pos': [array[3][1][0], array[3][1][1]]
-  }); //答案坐标点
-
-  g1.add_element({
-    'type': 'label',
-    'pos': [array[3][1][0] - 0.5, array[3][1][1] + 0.5],
-    'text': 'D'
-  });
-
-  g1.add_element({
-    'type': 'line_stroked',
-    'start': [array[0][1][0], array[0][1][1]],
-    'end': [array[1][1][0], array[1][1][1]]
-  });
-
-  g1.add_element({
-    'type': 'line_stroked',
-    'start': [array[1][1][0], array[1][1][1]],
-    'end': [array[2][1][0], array[2][1][1]]
-  });
-
-  g1.add_element({
-    'type': 'line_stroked',
-    'start': [array[2][1][0], array[2][1][1]],
-    'end': [array[3][1][0], array[3][1][1]]
-  });
-
-  g1.add_element({
-    'type': 'line_stroked',
-    'start': [array[3][1][0], array[3][1][1]],
-    'end': [array[0][1][0], array[0][1][1]]
-  });
-
-  g1.draw();
+  math_graphic.graphic(array_coordinate);
+  math_graphic.grid();
+  math_graphic.label();
+  lines_make(array_lines);
+  math_graphic.draw();
 };
+self.step_js = function(wrong_answer, step){
 
-
-self.step = function (wrong_answer, step) {
-  var steps = [
-    `Set ~PQ~ as the ~X~-axis, ~PS~ as the ~Y~-axis, then get all points at the coordinate.`,
-
-    `<div id='graph_6'></div>`,
-
-    `Find the centre of the masses `,
-
-    `The sum of the masses ~\\displaystyle\\sum m^{}_{i}=` + sum_m + ` ~`,
-
-    `~` + m1 + `~` + math_visuals.matrix.tex([
-      [array[0][1][0]],
-      [array[0][1][1]]
-    ]) + `~+` + m2 + `~` + math_visuals.matrix.tex([
-      [array[1][1][0]],
-      [array[1][1][1]]
-    ]) +
-    `~+` + m3 + `~` + math_visuals.matrix.tex([
-      [array[2][1][0]],
-      [array[2][1][1]]
-    ]) + `~+` + m4 + `~` + math_visuals.matrix.tex([
-      [array[3][1][0]],
-      [array[3][1][1]]
-    ]) +
-    `~=` + sum_m + `~` + math_visuals.matrix.tex([
-      [`\\bar x`],
-      [`\\bar y`]
-    ]),
-
-    `~\\therefore\\,~` + math_visuals.matrix.tex([
-      [m1 + `\\times` + (array[0][1][0] + `+` + m2 + `\\times` + array[1][1][0] + `+` + m3 + `\\times` + array[2][1][0] + `+` + m4 + `\\times` + array[3][1][0])],
-      [m1 + `\\times` + array[0][1][1] + `+` + m2 + `\\times` + array[1][1][1] + `+` + m3 + `\\times` + array[2][1][1] + `+` + m4 + `\\times` + array[3][1][1]]
-    ]) + `~=` + sum_m + `~` + math_visuals.matrix.tex([
-      [`\\bar x`],
-      [`\\bar y`]
-    ]),
-
-    `It's easy to find the ` + math_visuals.matrix.tex([
-      [`\\bar x`],
-      [`\\bar y`]
-    ]) + `~=~` + math_visuals.matrix.tex([
-      [coordinate[0]],
-      [coordinate[1]]
-    ]),
-
-    `~\\bold{a)}~ the distance between the center of mass and the side ~PQ~ is ~` + coordinate[0] + `~`,
-
-    `~\\bold{b)}~ the distance between the center of mass and the side ~PS~ is ~` + coordinate[1] + `~`,
-  ];
-  return steps[step];
 };
+	self.step = function(wrong_answer, step){
+		var steps = [
+			"First do this", 
+			"Then do that", 
+		];
+		return steps[step];
+	};
 
-self.step_js = function (wrong_answer, step) {
-  if (step === 0) {
-    return function () {
-      g6 = new graphic({
-        'input_element': "graph_6",
-        'low_x': coordinate_range[0],
-        'high_x': coordinate_range[1],
-        'low_y': coordinate_range[2],
-        'high_y': coordinate_range[3]
-      });
-      g6.add_element({
-        'type': 'grid'
-      });
-      g6.draw();
-    };
-  } else if (step === 1) {
-    return function () {
-      g6.add_element({
-        'type': 'dot',
-        'pos': [array[0][1][0], array[0][1][1]]
-      });
+	self.misconception = function(answer){
+		return 0; // Default
+	};
 
-      g6.add_element({
-        'type': 'label',
-        'pos': [array[0][1][0] - 0.5, array[0][1][1] + 0.5],
-        'text': 'A'
-      });
-
-      g6.add_element({
-        'type': 'dot',
-        'pos': [array[1][1][0], array[1][1][1]]
-      });
-
-      g6.add_element({
-        'type': 'label',
-        'pos': [array[1][1][0], array[1][1][1] + 0.5],
-        'text': 'B'
-      });
-
-      g6.add_element({
-        'type': 'dot',
-        'pos': [array[2][1][0], array[2][1][1]]
-      });
-
-      g6.add_element({
-        'type': 'label',
-        'pos': [array[2][1][0] - 0.5, array[2][1][1] + 0.5],
-        'text': 'C'
-      });
-
-      g6.add_element({
-        'type': 'dot',
-        'pos': [array[3][1][0], array[3][1][1]]
-      });
-
-      g6.add_element({
-        'type': 'label',
-        'pos': [array[3][1][0], array[3][1][1] + 0.5],
-        'text': 'D'
-      });
-
-      g6.add_element({
-        'type': 'dot',
-        'pos': [coordinate[0], coordinate[1]]
-      }); //答案坐标点
-
-      g6.add_element({
-        'type': 'label',
-        'pos': [coordinate[0], coordinate[1] + 0.5],
-        'text': 'G'
-      });
-
-      g6.add_element({
-        'type': 'line_stroked',
-        'start': [array[0][1][0], array[0][1][1]],
-        'end': [array[1][1][0], array[1][1][1]]
-      });
-
-      g6.add_element({
-        'type': 'line_stroked',
-        'start': [array[1][1][0], array[1][1][1]],
-        'end': [array[2][1][0], array[2][1][1]]
-      });
-
-      g6.add_element({
-        'type': 'line_stroked',
-        'start': [array[2][1][0], array[2][1][1]],
-        'end': [array[3][1][0], array[3][1][1]]
-      });
-
-      g6.add_element({
-        'type': 'line_stroked',
-        'start': [array[3][1][0], array[3][1][1]],
-        'end': [array[0][1][0], array[0][1][1]]
-      });
-      g6.draw();
-    };
-  }
-};
-
-self.misconception = function (answer) {
-  return 0; // Default
-};
-
-self.correct_answers = function () {
-  return [coordinate[0], coordinate[1]];
-};
+	self.correct_answers = function() {
+		return [];
+	};
--- JS/自己写的function.js
+++ JS/自己写的function.js
@@ -690,7 +690,7 @@ triangular_make = {
             self.post_load = function () {
                 var g;
                 g = new graphic({'input_element': array[array.length-1][0], 'low_x': math.min(x_array)-2,'high_x': math.max(y_array)+2,'low_y': math.min(y_array)-2,'high_y': math.max(y_array)+2,});
-                //g.add_element({'type': 'grid',});
+                g.add_element({'type': 'grid',});
                 g.add_element({'type': 'dot','pos': array[0]});
                 g.add_element({'type': 'dot','pos': array[1]});
                 g.add_element({'type': 'dot','pos': array[2]});
@@ -705,6 +705,32 @@ triangular_make = {
             };
         },
 };
+
+
+math_visuals.coordinate={
+
+    graphic:function(array=[['g1'],[-6,6],[-6,6]]){//重新开图一定要注意这句话//array的必要格式为[[第一个是id变量,函数内部使用id],[坐标点1],[坐标点2]]
+		g1 = new graphic({'input_element': array[0][0], 'low_x': array[1][0],'high_x': array[1][1],'low_y': array[2][0],'high_y': array[2][1],});
+    },
+    draw:function(){
+		g1.draw();
+    },
+    grid:function(){
+		g1.add_element({'type': 'grid',});
+    },
+    dot:function(array=[[0,0]]){
+		g1.add_element({'type': 'dot','pos': array[0]});
+    },
+    label:function(array=[[0.3,0.3],['O']]){//[[id][坐标点1][要输入的标示量]]
+		g1.add_element({'type': 'label','pos': array[0],'text': array[1]});
+    },
+    line:function(array=[[0,0],[0,1]]){
+		g1.add_element({'type': 'line','start': array[0],'end': array[1]});
+    },
+};
+
+self.post_load = function () {};
+self.step_js = function(wrong_answer, step){};
 
     <<
     <<
