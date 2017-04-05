7a8cf8926267a23f364eac8983c76088d62a34c4
shilei wei <shilei wei>
2017-04-05 14:12:32 +0800

    4.5

--- JS/自己写的function.js
+++ JS/自己写的function.js
@@ -792,9 +792,8 @@ function polynomial() {
         i-=1;
         if (typeof (array[0])=='string'){
             array.splice(0, 1);
-            i-=1;
         }
-    }
+    }//这部分有问题，哪里来的i，但是保留下来，因为下面写的还好，构思还行。
     (typeof (array[0])=='string')? array[0] : array[0]=math_visuals.num_form.head(array[0]);
 
     for (var i = 1; i < array.length - 1; i++) {
@@ -823,6 +822,64 @@ function polynomial() {
     }
     array_to_str = array.join(``);
     return array_to_str;
+}
+
+function polynomial() {
+	var array = Array.prototype.slice.call(arguments);
+	var array_to_str;
+	for (var j = 0; j < array.length - 1; j++) {
+		if (array[0] === 0) {
+			array.splice(0, 1);
+			if (typeof (array[0]) == 'string') {
+				array.splice(0, 1);
+			}
+		} else break;
+	}
+	array[0] = ((typeof (array[0]) == 'string') ? array[0] : math_visuals.num_form.head(array[0]));
+	for (var i = 1; i < array.length; i++) {
+		if (typeof (array[i]) == 'number') {
+			if (typeof (array[i + 1]) == `string`) {
+				if (array[i] === 0) {
+					array[i] = ``;
+					if (typeof (array[i + 1]) == 'string') {
+						array[i + 1] = ``;
+					}
+				} else array[i] = math_visuals.num_form.body(array[i]);
+
+			} else array[i] = math_visuals.num_form.end(array[i]);
+		}
+	}
+	array_to_str = array.join(``);
+	return array_to_str;
+}
+
+math_visuals['polynomial'] = function{
+	var array = Array.prototype.slice.call(arguments);
+	var array_to_str;
+	for (var j = 0; j < array.length - 1; j++) {
+		if (array[0] === 0) {
+			array.splice(0, 1);
+			if (typeof (array[0]) == 'string') {
+				array.splice(0, 1);
+			}
+		} else break;
+	}
+	array[0] = ((typeof (array[0]) == 'string') ? array[0] : math_visuals.num_form.head(array[0]));
+	for (var i = 1; i < array.length; i++) {
+		if (typeof (array[i]) == 'number') {
+			if (typeof (array[i + 1]) == `string`) {
+				if (array[i] === 0) {
+					array[i] = ``;
+					if (typeof (array[i + 1]) == 'string') {
+						array[i + 1] = ``;
+					}
+				} else array[i] = math_visuals.num_form.body(array[i]);
+
+			} else array[i] = math_visuals.num_form.end(array[i]);
+		}
+	}
+	array_to_str = array.join(``);
+	return array_to_str;
 }
 
 
