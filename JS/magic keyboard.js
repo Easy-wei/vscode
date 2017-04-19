4218a060d92047e75aecf37a94a6f6153d4f751f
shilei wei <shilei wei>
2017-04-19 14:39:45 +0800

    4.19

--- JS/magic keyboard.js
+++ JS/magic keyboard.js
@@ -18,49 +18,48 @@ variable_1 = math_tools.rand_int(1, 99);
 variable_2 = math_tools.rand_int(1, 99);
 
 // This function should return the question
-self.text = function() {
-	return `What is `+variable_1+` + `+variable_2+`?
+self.text = function () {
+	return `What is ` + variable_1 + ` + ` + variable_2 + `?
 	[answer  angle_measure="deg" answer keyboard='{{"1","2","3","4","5","6","7","8","9","0"},{".","e","i","+","-","*","/"},{"^","frac","root","cos","sin","tan","()"},{"left","right","del"}}' angle_measure="deg"]`;
-  	// Notice the [answer] above will output an answer box. More info can be found in another documentation module.
+	// Notice the [answer] above will output an answer box. More info can be found in another documentation module.
 };
 
 // This function will run after self.text is loaded into the DOM of the page. (Optional function)
-self.post_load = function(){
-};
+self.post_load = function () {};
 
 
 // This function should return the steps needed to explain to a student how they should complete an exercise
 // The first input is used to return a different explanation based on the answer of the student
 // The second is the the number of the step to be returned
-self.step = function(answer, step){
+self.step = function (answer, step) {
 	steps = [
-		"First do this", 
-		"Then do that", 
+		"First do this",
+		"Then do that",
 	];
 	return steps[step];
 };
 
 // This should return which misconception is detected, based on the students answer. 0 means we didn't detect a specific misconception.
-self.misconception = function(answer){
+self.misconception = function (answer) {
 	return 0; // Default
 };
 
 // This should return the correct answer, so we can test against the students input.
-self.correct_answers = function() {
-  	// There are 4 ways to return the answer.
-  	// In case we only have 1 answer, we can return just 1 number.
-  	return (variable_1 + variable_2);
-  	// Here self.ordered_answer doesn't matter.
-  	
-  	// In case self.ordered_answer == false (default) and there are multiple inputs, we can return like this
-	//return [1, 2];
-  	// Here both [1, 2] and [2, 1] are correct answers
-  
-  	// In case self.ordered_answer == true and we have more than one input
-	//return [1, 2];
-  	// Here only [1, 2] is correct, [2, 1] is not correct
+self.correct_answers = function () {
+	// There are 4 ways to return the answer.
+	// In case we only have 1 answer, we can return just 1 number.
+	return (variable_1 + variable_2);
+	// Here self.ordered_answer doesn't matter.
 
-  	// In case self.ordered_answer == true and we have more than one input and also different combinations of correct results
-  	//return [[1,2,3],[3,2,1]];
-  	// Here only [1,2,3] and [3,2,1] are correct answers, [2,1,3] for example is not correct
+	// In case self.ordered_answer == false (default) and there are multiple inputs, we can return like this
+	//return [1, 2];
+	// Here both [1, 2] and [2, 1] are correct answers
+
+	// In case self.ordered_answer == true and we have more than one input
+	//return [1, 2];
+	// Here only [1, 2] is correct, [2, 1] is not correct
+
+	// In case self.ordered_answer == true and we have more than one input and also different combinations of correct results
+	//return [[1,2,3],[3,2,1]];
+	// Here only [1,2,3] and [3,2,1] are correct answers, [2,1,3] for example is not correct
 };
--- JS/临时.js
+++ JS/临时.js
@@ -1,160 +1,44 @@
-  //self.order=False;
-  var vec1 = [math_tools.rand_int(-3, 3, [0]), math_tools.rand_int(-5, 5, [0])];
-  var vec2 = [math_tools.rand_int(-4, 4, [0]), math_tools.rand_int(-5, 5, [0])];
+  var v1 = math_tools.rand_int(1, 9);
+  var v2 = math_tools.rand_int(1, 9);
 
-  array_extremum = function (array) {
-      var x_abs_max = 5;
-      var y_abs_max = 5;
-      for (var i = 0; i < array.length; i++) {
-          if (x_abs_max < math.abs(array[i][0])) {
-              x_abs_max = math.abs(array[i][0]);
-          }
-          if (y_abs_max < math.abs(array[i][1])) {
-              y_abs_max = math.abs(array[i][1]);
-          }
-      }
-      return [x_abs_max, y_abs_max];
-  };
-
-
-  do {
-      var v1 = math_tools.rand_int(-5, 5, [0]);
-      var v2 = math_tools.rand_int(-5, 5, [0]);
-      var v3 = math_tools.rand_int(0, 2);
+  function inte(x, y = `x`, low = ``, hight = ``) {
+      return `\\displaystyle\\int_{` + low + `}^{` + hight + `}{` + x + `\\,\\,\\mathrm{d}` + y + `}`;
   }
-  while ((v1 + v2) % 2 !== 0);
 
-  f = function (x) {
-      return (x - v1) * (x - v2);
-  };
-
-  array = [[v1, 0],[v2, 0],[0, f(0)], [(v1 + v2) / 2, f((v1 + v2) / 2)]];
-
-  equation = `\\mathrm{y}=(x` + (-v1).signed() + `)(x` + (-v2).signed() + `)`;
-
-  points = [`when y=0`, `when x=0`, `the value of y minmum`];
+  var part1 = `x`;
+  var part1_1 = `1`;
+  var part2 = `-\\dfrac{\\mathrm{e}^{` + (-v1) + `x}}{` + v1 + `}`;
+  var part2_1 = `\\mathrm{e}^{` + (-v1) + `x}`;
+  var part3 = `x^{2}`;
+  var part3_1 = `2x`;
 
   self.text = function () {
-      return `The equation is ~` + equation + `~Find the point when ~\\mathrm{y}=0~<div id="graph" style="margin: auto;"></div>`;
+      return `Find~` + inte(part1 + part2_1, `x`, 0, `\\infty`) + `~, and ~` + inte(part3 + part2_1, `x`, 0, `\\infty`) + `~,
+	[answer keyboard='{{"1","2","3","4","5","6","7","8","9","0"},{".","e","i","+","-","*","/"},{"^","frac","root","cos","sin","tan","()"},{"x_var","y_var","s_var","left","right","del"}}']`;
   };
 
+  self.post_load = function () {};
 
-  self.post_load = function () {
-      var end = function (time, vars) {
-          return vars.end;
-      };
-
-      var g = new graphic({
-          'input_element': "graph",
-          'low_x': -array_extremum(array)[0] - 1,
-          'high_x': array_extremum(array)[0] + 1,
-          'low_y': -array_extremum(array)[1] - 1,
-          'high_y': array_extremum(array)[1] + 1,      });
-      g.add_element({          'type': 'grid'      });
-      //g.add_element({'type': 'line_head_arrow', 'end': vec1, 'color': 'green'});
-      //g.add_element({'type': 'line_head_arrow', 'end': vec2, 'color': 'red'});
-      //g.add_element({'type': 'line_head_arrow', 'end': end});
-      //g.add_element({'type': 'line_head_arrow', 'end': end1,'color':'yellow'});
-      turning_point = g.add_element({
-          'type': 'drag_point',
-          'pos': [4, 4],
-          'name': 'end',
-          'snap': 1
-      });
-      g.add_element({
-          'type': 'label',
-          'text': 'Turning point',
-          'pos': function () {
-              return turning_point.value;
-          },
-          'vertical_anchor': 'bottom'
-      });
-      y_intercept = g.add_element({
-          'type': 'drag_point',
-          'pos': [2, 2],
-          'name': 'end1',
-          'snap': 1
-      });
-      g.add_element({
-          'type': 'label',
-          'text': 'y intercept',
-          'pos': function () {
-              return y_intercept.value;
-          },
-          'vertical_anchor': 'bottom'
-      });
-      x_intercept_1 = g.add_element({
-          'type': 'drag_point',
-          'pos': [1, 1],
-          'name': 'end2',
-          'snap': 1
-      });
-      g.add_element({
-          'type': 'label',
-          'text': 'x intercept',
-          'pos': function () {
-              return x_intercept_1.value;
-          },
-          'vertical_anchor': 'bottom'
-      });
-      x_intercept_2 = g.add_element({
-          'type': 'drag_point',
-          'pos': [0, 0],
-          'name': 'end3',
-          'snap': 1
-      });
-      g.add_element({
-          'type': 'label',
-          'text': 'x intercept',
-          'pos': function () {
-              return x_intercept_2.value;
-          },
-          'vertical_anchor': 'bottom'
-      });
-  };
-
-
-  self.step_js = function (wrong_answer, step) {
-	var g1 = new graphic({
-          'input_element': "g1",
-          'low_x': -array_extremum(array)[0] - 1,
-          'high_x': array_extremum(array)[0] + 1,
-          'low_y': -array_extremum(array)[1] - 1,
-          'high_y': array_extremum(array)[1] + 1,      });
-    g1.add_element({'type': 'grid'});
-	g1.add_element({'type':'graph','function':f,'color':'#ff1744'});
-	g1.add_element({'type': 'circle', 'text': 'label', 'pos': array[0],'fill_color':'#76ff03','radius':'0.2'});
-	g1.add_element({'type': 'circle', 'text': 'label', 'pos': array[1],'fill_color':'#76ff03','radius':'0.2'});
-	g1.add_element({'type': 'circle', 'text': 'label', 'pos': array[2],'fill_color':'#76ff03','radius':'0.2'});
-	g1.add_element({'type': 'circle', 'text': 'label', 'pos': array[3],'fill_color':'#76ff03','radius':'0.2'});
-  };
+  self.step_js = function (wrong_answer, step) {};
 
   self.step = function (wrong_answer, step) {
       var steps = [
-		"<div id='g1'></div>",
-          `as you see all the dots are pointed on the coordinate`,
-		`When ~y=0~,therefore ~`+equation+`=0~ it's easy to find ~x_{1}=`+v1+`, x_{2}=`+v2+`~`,
-		`When ~x=0~,therefore ~\\mathrm{y}=(0`+(-v1).signed()+`)(0`+(-v2).signed()+`)=`+v1*v2+``+`~`,
-		`When ~x=\\dfrac{(`+v1+v2.signed()+`)}{2}, \\mathrm{y}_{_min}=`+array[3][1]+`~`,
+          `Begin by finding the integral from ~0~ to ~\\mathrm{s}~, and then consider their limits as ~\\mathrm{s}\\rightarrow\\infty~.`,
+          `For both integrals take ~\\dfrac{\\mathrm{d}v}{\\mathrm{d}x}=\\mathrm{e}^{-ax}~, so ~v=` + part2 + `~.`,
+          `~` + inte(part1 + part2_1, `x`, 0, `\\mathrm{s}`) + `=\\big[` + part1 + `\\times` + part2 + `\\big]^{\\mathrm{s}}_{0}-` + inte(part1_1 + `\\times` + part2, `x`, 0, `\\mathrm{s}`) + `~`,
+          `~-\\dfrac{1}{` + v1 + `}\\mathrm{se}^{-as}-\\dfrac{1}{` + v1 * v1 + `}\\mathrm{e}^{` + (-v1) + `s}+\\dfrac{1}{` + v1 * v1 + `}~.`,
+          `~~`,
       ];
       return steps[step];
   };
 
-  self.check_answer = function () {
-      if (turning_point.value[0] !== array[3][0] || turning_point.value[1] !== array[3][1]) {
-          return false;
-      }
-      if (y_intercept.value[0] !== array[2][0] || y_intercept.value[1] !== array[2][1]) {
-          return false;
-      }
 
-      var match = false;
-      if ((x_intercept_1.value[0] === array[0][0] && x_intercept_1.value[1] === array[0][1]) || (x_intercept_1.value[0] === array[1][0] && x_intercept_1.value[1] === array[1][1])) {
-          match = true;
-      }
+  self.misconception = function (answer) {
+      return 0; // Default
+  };
 
-      if (!match) {
-          return false;
-      }
-      return true;
+  var ans = new expression(`{x}+{y}`);
+
+  self.correct_answers = function () {
+      return ans;
   };
