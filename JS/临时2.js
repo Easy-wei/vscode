13a80a9907e0ba89a5884948a1160b60e1448162
shilei wei <shilei wei>
2017-04-24 16:33:05 +0800

    4.24

--- JS/临时2.js
+++ JS/临时2.js
@@ -1,32 +1,12 @@
-math_visuals["quadratic"] = {
-	ans_array: function (a, b, c) {
-		var k = math.pow(b, 2) - 4 * a * c;
-		if (k > 0) {
-			var k1 = math_visuals.root.integer_part(k);
-			var k2 = math_visuals.root.sqrt_part(k);
-			var com = math.gcd(b, k1, 2 * a);
-			if (math.pow(k1, 2) == k) {
-				return [math_visuals.fraction.str((-b + k1), 2 * a), math_visuals.fraction.str((-b - k1), 2 * a)];
-			}
-			b = b / com;
-			k1 = k1 / com;
-			var k3 = math.abs(a * 2 / com);
-			return (a > 0) ? (k3 == 1) ? [(-b) + math_visuals.num_form.body(k1) + k2, (-b) + math_visuals.num_form.body(-k1) + k2] : [`\\dfrac{` + (-b) + math_visuals.num_form.body(k1) + k2 + `}{` + k3 + `}`, `\\dfrac{` + (-b) + math_visuals.num_form.body(-k1) + k2 + `}{` + k3 + `}`] :
-				(k3 == 1) ? [(b) + math_visuals.num_form.body(-k1) + k2, (b) + math_visuals.num_form.body(k1) + k2] : [`\\dfrac{` + (-b) + math_visuals.num_form.body(k1) + k2 + `}{` + k3 + `}`, `\\dfrac{` + (b) + math_visuals.num_form.body(k1) + k2 + `}{` + k3 + `}`];
-		} else if (k === 0) {
-			return [math_visuals.fraction.str(-b, 2 * a)];
-		}
-	},
-	str: function (a, b, c, x = `x`) {
-		var j = math_visuals.quadratic.ans_array(a, b, c);
-		switch (j.length) {
-			case 1:
-				return x + `=` + j[0];
-			case 2:
-				return x + `^{}_{1}=` + j[0] + `, \\space ` + x + `^{}_{2}=` + j[1];
-		}
-	},
-	tex: function (a, b, c, x = `x`) {
-		return tex(math_visuals.quadratic.str(a, b, c, x = `x`));
-	},
-};
+function test(a, b) {
+	var x, y, z;
+	y = z = b = Math.pow(10, b);
+	x = a = a * b;
+	while (x > 0) {
+		z = x;
+		x = y % z;
+		y = z
+	};
+	return (a / z + "/" + b / z);
+}
+alert(test(0.75, 2));
