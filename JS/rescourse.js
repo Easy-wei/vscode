98ded7ba346adee16af5584351e35f4dc23683cd
shilei wei <shilei wei>
2017-04-24 11:47:04 +0800

    4.24

--- JS/rescourse.js
+++ JS/rescourse.js
@@ -407,44 +407,37 @@ math_visuals.root = {
   },
 };
 
-math_visuals["quadratic"] ={
-	test:function(n){
-		return (n==1)?``:(n==-1)?`-`:n;
-	},
-
-	str:function (a,b,c,x=`x`){//
-		var k=math.pow(b,2)-4*a*c;
-		if (k>0){ 
-			var k1=math_visuals.root.integer_part(k);
-			if (math.pow(k1,2)==k){
-				return x+`^{}_1=\\,`+math_visuals.fraction.str((-b+k1),2*a)+
-				`\\space\\space `+x+`^{}_2=\\,`+math_visuals.fraction.str((-b-k1),2*a);
-			}
-			var k2=math_visuals.root.sqrt_part(k);
-			var com=math_ngcd(b,k1,2*a);
-			b=b/com;
-			k1=k1/com;
-			var k3=math.abs(a*2/com);
-			return (a>0)?(k3==1)?x+`^{}_1=\\,`+(-b)+`+`+math_visuals.quadratic.test(k1)+k2+`\\space
-
-			`+x+`^{}_2=\\,`+(-b)+`-`+math_visuals.quadratic.test(k1)+k2:
-			x+`^{}_1=\\,\\dfrac{`+(-b)+`+`+math_visuals.quadratic.test(k1)+k2+`}{`+k3+`}\\space
-			`+x+`^{}_2=\\,\\dfrac{`+(-b)+`-`+math_visuals.quadratic.test(k1)+k2+`}{`+k3+`}`:
-			(k3==1)?x+`^{}_1=\\,`+(b)+`-`+math_visuals.quadratic.test(k1)+k2+`\\space
-			`+x+`^{}_2=\\,`+(b)+`+`+math_visuals.quadratic.test(k1)+k2:
-			x+`^{}_1=\\,-\\dfrac{`+(-b)+`+`+math_visuals.quadratic.test(k1)+k2+`}{`+k3+`}\\space
-			`+x+`^{}_2=\\,\\dfrac{`+(b)+`+`+math_visuals.quadratic.test(k1)+k2+`}{`+k3+`}`;
-		}
-		else if (k===0){
-			return x+`^{}_1=\\,`+math_visuals.fraction.str(-b,2*a);
-		}
-	},
-	
-	tex:function (a,b,c){
-		return tex(math_quadratic.str(a,b,c));
-	},
+math_visuals["quadratic"] = {
+    ans_array: function (a, b, c) {
+        var k = math.pow(b, 2) - 4 * a * c;
+        if (k > 0) {
+            var k1 = math_visuals.root.integer_part(k);
+            var k2 = math_visuals.root.sqrt_part(k);
+            var com = math.gcd(b, k1, 2 * a);
+            if (math.pow(k1, 2) == k) {
+                return [math_visuals.fraction.str((-b + k1), 2 * a), math_visuals.fraction.str((-b - k1), 2 * a)];
+            }
+            b = b / com;
+            k1 = k1 / com;
+            var k3 = math.abs(a * 2 / com);
+            return (a > 0) ? (k3 == 1) ? [(-b) + math_visuals.num_form.body(k1) + k2, (-b) + math_visuals.num_form.body(-k1) + k2] : [`\\dfrac{` + (-b) + math_visuals.num_form.body(k1) + k2 + `}{` + k3 + `}`, `\\dfrac{` + (-b) + math_visuals.num_form.body(-k1) + k2 + `}{` + k3 + `}`] :
+                (k3 == 1) ? [(b) + math_visuals.num_form.body(-k1) + k2, (b) + math_visuals.num_form.body(k1) + k2] : [`\\dfrac{` + (-b) + math_visuals.num_form.body(k1) + k2 + `}{` + k3 + `}`, `\\dfrac{` + (b) + math_visuals.num_form.body(k1) + k2 + `}{` + k3 + `}`];
+        } else if (k === 0) {
+            return [math_visuals.fraction.str(-b, 2 * a)];
+        }
+    },
+    str: function (a, b, c, x = `x`) {
+        switch (math_visuals.quadratic.ans_array(a, b, c).length) {
+            case 1:
+                return x + `=` + ans_array[0];
+            case 2:
+                return x + `^{}_{1}=` + ans_array[0] + `, \\space` + x + `^{}_{2}=` + ans_array[1];
+        }
+    },
+    tex: function (a, b, c, x = `x`) {
+        return tex(math_visuals.quadratic.str(a, b, c, x = `x`));
+    },
 };
-
 math_visuals['matrix'] = {
 	str: function(matrix){return `\\begin{bmatrix}` + matrix.map(function(line){return line.join(`&`);}).join(`\\cr`) + `\\end{bmatrix}`;},
 	tex: function(matrix){return tex(math_visuals.matrix.str(matrix));},
