d7cf99334a7b307c3346c410e3ec46e39a3998dd
shilei wei <shilei wei>
2017-04-26 20:18:05 +0800

    4.26

--- JS/自己写的function.js
+++ JS/自己写的function.js
@@ -832,4 +832,4 @@ math_tools["points_to_equation.function"] = function (ponits, x) {
     return sum
 }
 //手动添加magic keyboard方法
-  [answer keyboard='{{"1","2","3","4","5","6","7","8","9","0"},{".","e","i","+","-","*","/"},{"^","frac","root","cos","sin","tan","()"},{"y_var","s_var","left","right","del"}}']`;
+	[answer keyboard='{{"1","2","3","4","5","6","7","8","9","0"},{".","e","i","+","-","*","/"},{"^","frac","root","cos","sin","tan","()"},{"x_var","y_var","left","right","del"}}' angle_measure="deg"]`;
