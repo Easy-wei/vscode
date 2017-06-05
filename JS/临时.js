fb0028d177c80712120550a748185f7ca807d49d
Easy we <easy-we@outlook.com>
2017-06-05 09:33:58 +0800

    6.5

--- JS/临时.js
+++ JS/临时.js
@@ -1,6 +1,7 @@
-  var g1= new g1raphic({'input_element':'g2','low_x':coord[0][0],'hig1h_x':coord[0][1],'low_y':coord[1][0],'hig1h_y':coord[1][1],});
-  g1.add_element({'type':'g1rid'});
-  g1.add_element({'type':'g1raph', 'function':f,'min_x':0});
-  g1.add_element({'type':'dot', 'pos':[v1,f(v1)]});
-  g1.add_element({'type':'label', 'text':'P','pos':[v1+0.7,f(v1)]});
-  g1.add_element({'type':'g1raph', 'function':f_1, 'max_x':v1,'min_x':0,'fill':'true','fill_baseline':f});  
+  g1.add_element({'type':'label', 'text':1, 'pos':[1,-0.5]});
+  g1.add_element({'type':'label', 'text':'s', 'pos':[6,-0.5]});
+  
+  g1.add_element({'type':'graph', 'function':f,'min_x':1, 'max_x':6, 'fill':'true','fill_baseline':0,'color':'#CE93D8'});
+  g1.add_element({'type':'graph', 'function':f,'min_x':0});
+  g1.add_element({'type':'line', 'start':[1,0],'end':[1,f(1)]});
+  g1.add_element({'type':'line', 'start':[6,0],'end':[6,f(6)]});
