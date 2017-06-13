9959a0c0532fcb07bb27cbf6642a9daa86bc1e18
Easy we <easy-we@outlook.com>
2017-06-13 17:46:21 +0800

    6.13

--- JS/自己写的function.js
+++ JS/自己写的function.js
@@ -864,3 +864,17 @@ var f=function (x){
 
 
 var br = function (x) { return ((x<0)?`(`+x+`)`:x);};
+
+
+var frac = function (a, b) {
+    var com = math_tools.gcd(math.abs(a), math.abs(b));
+    if (b === 0) {
+        throw "the denominator can not be 0";
+    }
+    a = a / com;
+    b = b / com;
+    var j = math.abs(a) % math.abs(b);
+    var k = (math.abs(a) - math.abs(j)) / math.abs(b);
+    return (a * b >= 0) ? (j === 0) ? k : `\\frac{` + math.abs(a) + `}{` + math.abs(b) + `}` :
+        (j === 0) ? -k : `-\\frac{` + math.abs(a) + `}{` + math.abs(b) + `}`;
+};
