b9bd2e8473f96c298cf5bfe2312e461f3ab92ecc
Easy we <easy-we@outlook.com>
2017-06-15 22:31:51 +0800

    6.15

--- JS/临时.js
+++ JS/临时.js
@@ -1,45 +1,80 @@
 var inte=function(x,a=``,b=``,y=`x`){ return `\\displaystyle\\int^{`+b+`}_{`+a+`}{`+x+`}\\mathrm{d}`+y;};
 
-var v1 = math_tools.rand_int(1, 3);
-var v4 = math_tools.rand_int(1, 4);
+var v1 = 2;
+var v2 = math_tools.rand_int(1, 4);
 do{
-var low_limit = math_tools.rand_int(1, 6);
-var h_limit = math_tools.rand_int(1, 5);
+  var v3 = math_tools.rand_int(-2, 2);
+  var v4 = math_tools.rand_int(-2, 2);
 }
-while(low_limit>=h_limit);
+while(v3>=v4);
 
-var f_equation = `\\sqrt{x`+math_visuals.num_form.end(v1*v1)+`}`;
-var f_equation_1 = math_visuals.polynomial(v1*v1,`x^{`+2*v4+`}`);
-var F_equation = math_visuals.polynomial(math_visuals.fraction.str(v1*v1,(2*v4+1)),`x^{`+(2*v4+1)+`}`);
+var low_limit = 0;
+var h_limit = v2;
 
-var F_s = function(x){ return v1*v1*Math.pow(x,(2*v4+1)) ;};
-var F_n = function(x){ return math_visuals.fraction.str(F_s(x),(2*v4+1));};
+var f_equation = math_visuals.polynomial(v2,`x`);
+var f_equation2 = math_visuals.polynomial(`x^{2}`);
+var f_equation_1 = math_visuals.polynomial(`-x^{4}`,v2*v2,`x^{2}`);
+var F_equation = math_visuals.polynomial(math_visuals.fraction.str(1,5),`x^{5}`,math_visuals.fraction.body(v2*v2,3),`x^{3}`);
 
-var F = function(x) { return v1*v1/(2*v4+1)*Math.pow(x,2*v4+1)*Math.PI;};
+
+var f1 = function(x) {return v1*x ;};
+var f2 = function(x) {return x*x/2 ;};
+var f1_1 = function(x) {return -v1*x ;};
+var f2_1 = function(x) {return -x*x/2 ;};
+
+var F_s = function(x){ return 3*Math.pow(x,5)+v2*v2*5*Math.pow(x,3);};
+var F_n = function(x){ return math_visuals.fraction.str(F_s(x),15);};
+
+var F = function(x) { return (Math.pow(x,5)/5+v2*v2/3*Math.pow(x,3))*Math.PI;};
 
 self.text = function() {
-	return `Find the volume generate when the region under the graph of ~y=`+f_equation+`~
-between ~x=0~ and ~x=`+h_limit+`~ is roated throught ~360~° about the ~x~-axis.
-[answer keyboard='{{"1","2","3","4","5","6","7","8","9","0"},{".","e","i","+","-","*","/","pi"},{"^","frac","cos","sin","tan","root","ln","()"},{"x_var","k_var","left","right","del"}}']
+	return `The region enclosed between the graph of ~y=`+f_equation+`~ and ~y=`+f_equation2+`~ is denoted by ~\\mathrm{R}~.
+Find 
+
+
+~a)~  the ~x~-axis [answer]
+~b)~  the ~y~-axis [answer]
 `;
 };
 
 self.text_js = function(){
 };
 
+var coord = [[-(2*v1*v1+2)*0.618,(2*v1*v1+2)*0.618],[-(2*v1*v1+2),2*v1*v1+2]];
+
 self.step_js = function(misconception, step){
+  var g= new graphic({'input_element':'g1','low_x':coord[0][0],'high_x':coord[0][1],'low_y':coord[1][0],'high_y':coord[1][1],});
+  //g.add_element({'type':'grid'});
+
+  g.add_element({'type':'graph', 'function':f1,'min_x':0, 'max_x':4, 'fill':'true','fill_baseline':f1_1,'color':'#FF4081'});
+  g.add_element({'type':'graph', 'function':f2,'min_x':0, 'max_x':4, 'fill':'true','fill_baseline':f2_1,'color':'#009688'});
+  g.add_element({'type':'graph', 'function':f1,'min_x':0});
+  g.add_element({'type':'graph', 'function':f2,'min_x':0});
+  g.add_element({'type':'graph', 'function':f2_1,'min_x':0});
+  g.add_element({'type':'graph', 'function':f1_1,'min_x':0});
+  
+  g.add_element({'type':'line_head_arrow', 'start':[-2,0] ,'end':[5,0]});
+  g.add_element({'type':'line_head_arrow', 'start':[0,-10] ,'end':[0,10]});
+  g.add_element({'type':'label', 'pos':[-0.5,9] ,'text':`y`});
+  g.add_element({'type':'label', 'pos':[4.5,-0.4] ,'text':`x`});
 };
 
 self.step = function(misconception, step){
 	var steps = [
-	  `The required volume is ~V~, where`,
-	  `~V=`+inte(`\\pi{y^{2}}`,low_limit,h_limit)+`~`,
-	  `~=\\pi`+inte(`(`+f_equation+`)^{2}`,low_limit,h_limit)+`~`,
+	  `Take ~f(x)=`+f_equation+`~ and ~g(x)=`+f_equation2+`~`,
+	  `~a)~ about the ~x~-axis.`,
+	  `<div id='g1'></div>`,
+	  `As you see the the graph, we need to find the pink volume`,
+	  `Add the pink volume and the green volume, we will get the volume generated when ~y=`+f_equation+`~ is rotated ~360~°`,
+	  `The green volume is generated when ~y=`+f_equation2+`~ is rotated ~360~°`,
+	  `So the pink volume ~=`+inte(`f(x)^{2}\\pi`,low_limit,h_limit)+`-`+inte(`g(x)^{2}\\pi`,low_limit,h_limit)+`~`,
+	  `~V=`+inte(`(f(x)^{2}-g(x)^{2})\\pi`,low_limit,h_limit)+`~`,
 	  `~=\\pi`+inte(`(`+f_equation_1+`)`,low_limit,h_limit)+`~`,
 	  `~=\\pi\\Big[`+F_equation+`\\Big]^{`+h_limit+`}_{`+low_limit+`}~`,
-	  `~=\\Big(`+F_n(h_limit)+`-`+F_n(low_limit)+`\\Big)\\pi~`,
-	  `~=`+math_visuals.fraction.str(F_s(h_limit)-F_s(low_limit),2*v4+1)+`\\pi~`,
-	  `The volume of the solid is ~`+math_visuals.fraction.str(F_s(h_limit)-F_s(low_limit),2*v4+1)+`\\pi~`,
+	  `~=\\Big(`+F_n(h_limit)+`-(`+F_n(low_limit)+`)\\Big)\\pi~`,
+	  `~=`+math_visuals.fraction.str(F_s(h_limit)-F_s(low_limit),15)+`\\pi~`,
+	  `The volume of the solid is ~`+math_visuals.fraction.str(F_s(h_limit)-F_s(low_limit),15 )+`\\pi~`,
+	  `In the same way it's easy to find the volume generated when ~\\mathrm{R}~ is rotated about ~y~-axis ~=`+inte(`(x_{1}-x_{2})`)+`~`,
 	];
 	return steps[step];
 };
@@ -50,5 +85,5 @@ self.misconception = function(answer){
 };
 
 self.correct_answers = function() {
-	return (F(h_limit)-F(low_limit));
+	return [(F(h_limit)-F(low_limit))];
 };
--- python/crawel/url7.py
+++ python/crawel/url7.py
@@ -4,7 +4,7 @@ import http.cookiejar
  
 
 def saveFile(data):
-    save_path = 'd:\'
+    save_path = 'd:\\url.txt'
     f_obj = open (save_path,'wb') #wb 表示打开方式
     f_obj.write(data)
     f_obj.close()
@@ -12,10 +12,10 @@ def saveFile(data):
 # head: dict of header
 
 def makeMyopener(head = { 
-    'Connection': 'Keep-Alive',
-    'Accept': 'text/html, application/xhtml+xml, */*',
-    'Accept-language': 'en-US,en;q=0.8,zh-Hans-CN; q = 0.5 ,zh-Hans; q = 0.3'
-    'User-Agent': 'Mozilla/5.0(windows NT 6.3; WOW64; Trident/7.0; rv:11.0) likeGecko'  
+    'Connection': 'keep-alive',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-language':'zh-CN,zh;q=0.8,ja;q=0.6,en-US;q=0.4,en;q=0.2,en-GB;q=0.2',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
 }):
     cj = http.cookiejar.CookieJar()
     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
@@ -28,9 +28,25 @@ def makeMyopener(head = {
 
 oper = makeMyopener()
 
-url = 'http://www.baidu.com/'
+url = 'https://www.baidu.com/'
 
 uop = oper.open(url, timeout = 1000)
 
 data = uop.read()
-print(data.decode())
+print('ask')
+saveFile(data)
+print(data)
+
+"""
+GET / HTTP/1.1
+Host: www.baidu.com
+Connection: keep-alive
+Cache-Control: max-age=0
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+DNT: 1
+Accept-Encoding: gzip, deflate, sdch, br
+Accept-Language: zh-CN,zh;q=0.8,ja;q=0.6,en-US;q=0.4,en;q=0.2,en-GB;q=0.2
+Cookie: BDUSS=zRVR2l0eTdBUDQ3aXJQcHhxUzlSNDlILXJwflpudEpHVmJzYjAtRjF-UGtBRUZaSVFBQUFBJCQAAAAAAAAAAAEAAABpNnpN2K3YrUVhc3nYrditAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAORzGVnkcxlZb; BAIDUID=DFE10D2D446D16C96DE4511FE928FA0D:FG=1; BIDUPSID=DFE10D2D446D16C96DE4511FE928FA0D; PSTM=1497533024; H_PS_645EC=c027P1jgMdzqJn1h8aI%2BGXlTvLZxMUN3%2FMuXMK2Wd09sIVeGf8jEESy0I8A; BD_CK_SAM=1; PSINO=5; BD_HOME=1; H_PS_PSSID=1421_13548_21089_18559_17001_20927; BD_UPN=12314753; sugstore=0
+"""
