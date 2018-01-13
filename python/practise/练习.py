b894a185659c976113585f115d5556b037ef93ff
easy-wei <easy-we@outlool.com>
2018-01-13 23:22:33 +0800

    0113

--- python/practise/练习.py
+++ python/practise/练习.py
@@ -1,6 +1,6 @@
 from math import pi as PI
 """
-def circleare(a):
+def circleare(a):                 #求直径
     if isinstance(a,int) or isinstance(a,float):
         return a**2*pi
     else :
@@ -10,40 +10,43 @@ x=int(input('input the circle radius'))
 print (circleare(x))
 
 
-def demo(*a):
-    avg=sum(a)/len(a)
-    g=[i for i in a if i>avg]
+def demo(*a):  # 输出均值和所有大于均值的数
+    avg=sum(a)/len(a)        #均值
+    g=[i for i in a if i>avg]         #所有大于均值的数
     return (avg,g)
 
 print (demo(1,2,3,4))
 
 
 
-def demo(a):
+def demo(a):        #统计字符串里大小写的数量
     result=[0,0]
     for i in a :
         if 'a'<=i<='z':
-            result[0]+=1
+            result[0]+=1 #  统计小写数量       
         elif 'A'<=i<='Z':
             result[1]+=1
     return result
 print (demo('aDaaaadsdfdfdsfasdFADSFASDFDSfdadsfafsDFADd '))
 
 
-
+# 找到一个list，从中第k个数截取，然后前k个数排序，后n-k个数排序，然后重组。
 def demo(list,k):
     if k>len(list):
         print ('k is over the lenght of list')
-    x=list[:k]
-    x.reverse()
-    y=list[k:]
-    y.reverse()
-    r= x+y
-    r.reverse()
+    x=list[:k]  # 截取前k个数
+    x.reverse() # 前k个数逆序。
+    y=list[k:]  # 截取后n-k个数。
+    y.reverse() # 后n-k个数逆序。
+    r= x+y      # 重组
+    r.reverse() # 重组后再逆序，图个啥！
     return r
 
 a=[i for i in range(1,21)]
 print(demo(a,5))
+
+
+
 
 斐波那契
 def demo(t):
@@ -192,159 +195,162 @@ def demo(*x):
 
 print (demo(1,2,3,5,6,3,32,56,67,34,5,876,654))
 
-"""
+
 # filename:myarray.py
-#function description: array and its operating
+# function description: array and its operating
 
 import types
 
+
 class MyArray:
-    """all the elements in this array must be numbers"""
-    __value=[]
-    __size=0
+     all the elements in this array must be numbers
+    __value = []
+    __size = 0
 
     def __IsNumber(self, n):
-        if type(n)!=types.ComplexType and type(n)!=types.FloatType and type(n)!=types.IntType and type(n)!=types.LongType:
+        if type(n) != types.ComplexType and type(n) != types.FloatType and type(n) != types.IntType and type(n) != types.LongType:
             return False
         return True
 
-    
-    def __init__(self,*args):
+    def __init__(self, *args):
         for i in args:
             if not self.__IsNumber(args):
-                print ("ALL elements must be numbers")
-                return 
-        self.__value=[]
+                print("ALL elements must be numbers")
+                return
+        self.__value = []
         for j in args:
             self.__value.append(j)
-        self.__size=len.(args)
-        
+        self.__size = len.(args)
 
-    def __add__(self,n):
+    def __add__(self, n):
         if not self.__IsNumber(n):
-            print ('+operating with ',type(n),'and number type is not supported.')
-            return 
-        b=MyArray()
+            print('+operating with ', type(n),
+                  'and number type is not supported.')
+            return
+        b = MyArray()
         for v in self.__value:
-            b.__value.append(v+n)
+            b.__value.append(v + n)
         return b
 
-
-    def __sub__(self,n):
+    def __sub__(self, n):
         if not self.__IsNumber(n):
-            print ('-operating with ',type(n),'and number type is not supported.')
-            return 
-        b=MyArray()
+            print('-operating with ', type(n),
+                  'and number type is not supported.')
+            return
+        b = MyArray()
         for v in self.__value:
-            b.__value.append(v-n)
+            b.__value.append(v - n)
         return b
 
-    def __mul__(self,n):
+    def __mul__(self, n):
         if not self.__IsNumber(n):
-            print ('*operating with ',type(n),'and number type is not supported.')
-            return 
-        b=MyArray()
+            print('*operating with ', type(n),
+                  'and number type is not supported.')
+            return
+        b = MyArray()
         for v in self.__value:
-            b.__value.append(v*n)
+            b.__value.append(v * n)
         return b
 
-    def __div__(self,n):
+    def __div__(self, n):
         if not self.__IsNumber(n):
-            print ('/operating with ',type(n),'and number type is not supported.')
-            return 
-        b=MyArray()
+            print('/operating with ', type(n),
+                  'and number type is not supported.')
+            return
+        b = MyArray()
         for v in self.__value:
-            b.__value.append(v/n)
+            b.__value.append(v / n)
         return b
 
-
-    def __mod__(self,n):
+    def __mod__(self, n):
         if not self.__IsNumber(n):
-            print ('%operating with ',type(n),'and number type is not supported.')
-            return 
-        b=MyArray()
+            print('%operating with ', type(n),
+                  'and number type is not supported.')
+            return
+        b = MyArray()
         for v in self.__value:
-            b.__value.append(v%n)
+            b.__value.append(v % n)
         return b
 
-
-    def __pow__(self,n):
+    def __pow__(self, n):
         if not self.__IsNumber(n):
-            print ('**operating with ',type(n),'and number type is not supported.')
-            return 
-        b=MyArray()
+            print('**operating with ', type(n),
+                  'and number type is not supported.')
+            return
+        b = MyArray()
         for v in self.__value:
             b.__value.append(v**n)
         return b
 
-
     def __len__(self):
         return len(self.__value)
 
-#for:m
-#when use the object as a statement diectly the function will be called 
+# for:m
+# when use the object as a statement diectly the function will be called
     def __repr__(self):
-        #equaivalent to return "self.__value"
+        # equaivalent to return "self.__value"
         return repr(self.__value)
-    #for : print x
+    # for : print x
+
     def __str__(self):
         return str(self.__value)
-    
-    def append(self,v):
+
+    def append(self, v):
         if not self.__IsNumber(v):
-            print ('Only number can be appended')
+            print('Only number can be appended')
             return
         self.__value.append(v)
-        self.__size+=1
+        self.__size += 1
 
     def __getitem__(self, index):
-        if self.__IsNumber(index) and 0<=index<=self.__size:
+        if self.__IsNumber(index) and 0 <= index <= self.__size:
             if self.__IsNumber(v):
-                self.__value[index]=v
+                self.__value[index] = v
             else:
                 print(v, 'is not a number')
         else:
-            print ( index, 'is not a number or out of range')
+            print(index, 'is not a number or out of range')
 
-
-    #member test.support  the keyword 'in'
+    # member test.support  the keyword 'in'
 
     def __contains__(self, v):
         if v in self.__value:
             return True
         return False
 
-    
-    #dot product
-    def dot(self,v):
-        if not isinstance(v,MyArray):
-            print (v,'must be an instance of MyArray')
+    # dot product
+    def dot(self, v):
+        if not isinstance(v, MyArray):
+            print(v, 'must be an instance of MyArray')
             return
-        if len(v)!=self.__size:
-            print ('The size must be equal')
+        if len(v) != self.__size:
+            print('The size must be equal')
             return
-        b=MyArray()
-        for m, n in zip(v.__value,self.__value):
-            b.__value.append(m*n)
+        b = MyArray()
+        for m, n in zip(v.__value, self.__value):
+            b.__value.append(m * n)
             return sum(b.__value)
 
-    #equal to 
-    def __eq__(self,v):
-        if not isinstance(v,MyArray):
-            print(v,'must be an isinstance of MyArray')
+    # equal to
+    def __eq__(self, v):
+        if not isinstance(v, MyArray):
+            print(v, 'must be an isinstance of MyArray')
             return
-        if cmp(self.__value,v.__value)==0:
+        if cmp(self.__value, v.__value) == 0:
             return True
         return False
-    #less than
-    def __lt__(self,v):
-        if not isinstance(v,MyArray):
-            print (v,'must be an isinstance of MyArray')
+    # less than
+
+    def __lt__(self, v):
+        if not isinstance(v, MyArray):
+            print(v, 'must be an isinstance of MyArray')
             return
-        if self.__value<v.__value:
+        if self.__value < v.__value:
             return True
         return False
 
-if __name__=='__main__':
-    print ('Please use me as a module')
+
+if __name__ == '__main__':
+    print('Please use me as a module')
+"""
 
