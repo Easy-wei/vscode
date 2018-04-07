10680a764e92c2d27d6d73f1c238bae8d096411a
Easy-wei <easy-we@outlook.com>
2018-04-07 23:24:47 +0800

    0407

--- python/crawel/url4.py
+++ python/crawel/url4.py
@@ -1,6 +1,31 @@
-import requests
-requests.post("http://httpbin.org/post")
-requests.put("http://httpbin.org/put")
-requests.delete("http://httpbin.org/delete")
-requests.head("http://httpbin.org/get")
-requests.options("http://httpbin.org/get")
+#coding=utf-8
+import urllib.request  #3之前的版本直接用urllib即可，下同 #该模块提供了web页面读取数据的接口，使得我们可以像读取本地文件一样读取www或者ftp上的数据
+import re
+import os
+ 
+def getHtml(url):
+    page = urllib.request.urlopen(url);
+    html = page.read();
+    return html;
+ 
+def getImg(html):
+    imglist = re.findall('img src="(http.*?)"',html)#1 #http.*?表示非贪婪模式的匹配，只要符合http就匹配完成，不再看后面的内容是否匹配，即在能使整个匹配成功的前提下，使用最少的重复
+    return imglist
+ 
+html = getHtml("http://blog.sina.com.cn/s/blog_497ccf1e0100rd87.html").decode("utf-8");
+imagesUrl = getImg(html);
+ 
+if os.path.exists("e:/code/imageDownload") == False:
+    os.mkdir("e:/code/imageDownload");
+     
+count = 0; #文件的起始名称为 0
+for url in imagesUrl:
+    print(url)
+    if(url.find('.') != -1):#2
+        name = url[url.find('.',len(url) - 5):];
+        bytes = urllib.request.urlopen(url);
+        f = open("e:/code/imageDownload/"+str(count)+name, 'wb');  #代开一个文件，准备以二进制写入文件
+        f.write(bytes.read());#write并不是直接将数据写入文件，而是先写入内存中特定的缓冲区
+        f.flush();#将缓冲区的数据立即写入缓冲区，并清空缓冲区
+        f.close();#关闭文件
+        count+=1;
