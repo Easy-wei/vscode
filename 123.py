f9b9f780aa39d082a063ab4561c546386d635467
Easy-wei <Easy-we@outlook.com>
2018-05-30 20:42:56 +0800

    05.30

--- 123.py
+++ 123.py
@@ -4,39 +4,7 @@ ab0f9d7dd4dc4f29c6b454c74beeeb587d02dd7c  token 令牌
 312d79cf1088a64b89cc8087687e12915af46fcb    token 令牌
 7457f3d31f0b5694919c327f5699ce15 gist_id
 
-
-
-def textFormat(dict):
-    for key in dict:
-        #print('{0:<10}{1:<10}{2:<10}{3}'.format(key,dict[key][day],dict[key][month],dict[key][total]))
-        print('{0:<40}{1:<10}'.format(key,dict[key][%s]=%))
 """
-import requests
-import json
-import time
-import threading
-import collections # 用有序字典
-
-result = collections.OrderedDict()#有序字典
-url = 'http://tv.honganrobots.com/get_count'
-
-def getCount(url):
-    times = time.asctime()
-    response = requests.get(url).json()
-    result[times] = response
-
-getCount(url)
-for key in result:
-    #print(type(result[key]))
-    print('{0:<25}{1}'.format(key,result[key]["month"]))
-    
-def storageText(a):
-    with open ('红岸水滴筐数统计.txt','a+') as f1:
-        f1.write(a)
-
-
-#if __name__ == "__main__":
-#    getCount()
 
 
 
--- python/crawel/红岸水滴筐数阻塞式.py
+++ python/crawel/红岸水滴筐数阻塞式.py
@@ -12,15 +12,23 @@ def getCount(url):
     response = requests.get(url).json()
     result[times] = response
 
-getCount(url)
-for key in result:
-    #print(type(result[key]))
-    print('{0}{1}'.format(key,result[key]["month"]))
-    
 def storageText(a):
     with open ('红岸水滴筐数统计.txt','a+') as f1:
-        f1.write(a)
+        for key in result:    #print(type(result[key]))
+            print('{0:<25}{1:<10}{2:<10}{3:<10}'.format(key,result[key]["day"],result[key]["month"],result[key]["total"]),file=f1)
 
+time = int(time.time())
+
+while ((time.time()-time)>60):
+    time = time.time()
+    getCount(url)
+
+storageText(result)
+
+
+# getCount(url)
+
+# storageText(result)
 
 #if __name__ == "__main__":
 #    getCount()
