afcb38bc84b8cd3296d1a3adb10d69acac76fc30
Easy-wei <Easy-we@outlook.com>
2018-07-28 20:39:41 +0800

    07.28

---
+++ python/crawel/短期激增报警.py
@@ -0,0 +1,33 @@
+import requests
+import time
+import sched
+
+schedule = sched.scheduler(time.time,time.sleep)
+
+url = 'http://redash.honganhome.com/api/queries/1573/results.json?api_key=xlDxNClAVnewK6I2xmW18MtsqHvrgsuMItt25ssP'
+#print(requests.get(url).json())
+result1 = requests.get(url).json()
+
+def get_result(url1): #用url1 表明是形参，防止和url歧义
+    return requests.get(url1).json()
+
+
+def compass (a,b):
+    c= []
+    for i in range(0,len(b['query_result']['data']['rows'])):
+        sub = b['query_result']['data']['rows'][i]['codenum']- a['query_result']['data']['rows'][i]['codenum']
+        if sub >=10:
+            # 把【device_id， 筐数，1min报错数量】以list形式写入到c，然后return
+            c.append([b['query_result']['data']['rows'][i]['device_id'],b['query_result']['data']['rows'][i]['basket_count'],sub])
+    a = b # 把result1更新了
+    return c 
+
+time.sleep(360)
+print('aaa')
+print (compass(result1,get_result(url)))
+
+
+
+#print (result1['query_result']['retrieved_at'],result1['query_result']['data']['rows'][0]['codenum'],result1['query_result']['data']['rows'][0]['device_id'])
+
+
