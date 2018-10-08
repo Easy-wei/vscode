f68d2d4059e5b363eda2c0a2f244f8801bd6779c
Easy-wei <Easy-we@outlook.com>
2018-10-08 23:40:38 +0800

    111

--- 大数据比赛/read.py
+++ 大数据比赛/read.py
@@ -1,5 +1,18 @@
 import xlrd
+import pandas as pd 
 
-excelfile = xlrd.open_workbook(r'C:\Users\Easy-\Desktop\比赛处理后的数据.xlsx')
 
-print (excelfile.sheet_by_name("track_data2"))
+def to_list(nrows): 
+    data = xlrd.open_workbook(r'C:\Users\Easy-\Desktop\100个数据测试用.xlsx') 
+    row_num = data.sheets()[0].nrows
+    list = []
+    for i in range (1,row_num):   # 不要列名字
+        list.append(data.sheets()[0].cell(rowx = i , colx = nrows ).value)
+    return list 
+
+list_time = to_list(0)
+list_state = to_list(1)
+list_longtitue = to_list(2)
+list_latitue = to_list(3)
+
+print(list_longtitue,len(list_longtitue))
