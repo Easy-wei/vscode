2da0f0ec0c68d349afb193f0ad31fef14a1e0804
Easy-wei <easy-we@outlook.com>
2018-10-10 22:43:16 +0800

    10.10

--- 大数据比赛/post2.py
+++ 大数据比赛/post2.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 url = "http://yingyan.baidu.com/api/v3/track/addpoints"
 
@@ -7,16 +6,17 @@ url = "http://yingyan.baidu.com/api/v3/track/addpoints"
 payload = {
     "ak": "9GctB73jNG4AGsH6RldMqnCvGzafFylt",
     "service_id": "205445",
-    "point_list": [{"entity_name": "3",
-                    "latitude": 31.767862,
-                    "longitude": 117.267296,
-                    "loc_time": 1525125410,
-                    "coord_type_input": "wgs84",},
-                   {"entity_name": "3",
-                    "latitude": 31.767862,
-                    "longitude": 117.267296,
-                    "loc_time": 1525125440,
-                    "coord_type_input": "wgs84",}]
+    "point_list": [
+        {"entity_name": "3",
+         "latitude": "31.767862",
+         "longitude": "117.267296",
+         "loc_time": "1525125410",
+         "coord_type_input": "wgs84"},
+        {"entity_name": "3",
+         "latitude": "31.767862",
+         "longitude": "117.267296",
+         "loc_time": "1525125440",
+         "coord_type_input": "wgs84"}]
 }
 
 response = requests.post(url, data=payload)
